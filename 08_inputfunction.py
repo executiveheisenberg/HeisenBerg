8d5de089c38c42ab1be5399d7da73ae593e6a32c
executiveheisenberg <105383512+executiveheisenberg@users.noreply.github.com>
2022-05-11 23:05:28 +0530

    Add files via upload

---
+++ 08_inputfunction.py
@@ -0,0 +1,29 @@
+#Author: HeisenBerg
+#Shows your age
+a=input("Enter your name ") #asking you to enter
+b=input("Enter your year of birth ")#make this integer
+c=input("Enter current year ")#integer as well as this is also number
+b=int(b)#made an integer
+print(type(b))#shows type of variable
+c=int(c)#made an integer
+print(type(c))#shows type of variable
+print("Your name is ",a) #show the answer
+print("Your age is ", c-b) 
+n=input("Next exampe, press enter")
+#example of average of two numbers
+a=input("Enter a number ")
+b=input("Enter another one ")
+a=int(a)
+b=int(b)
+avg=(a+b)/2
+avg=float(avg)
+print("Average of these numbers is ",avg)
+s=input("Next example of square root of input number")
+#example of square of an input number
+a=input("Enter a number ")
+a=int(a)
+#b=input("Enter another one ")
+b=a*a
+b=float(b)
+print("Square of entered number is ", b)
+e=input("Thank you, Press enter to exit")
